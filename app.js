e6382673da665cbad4980f97a8942bb4f21ea1b6
Lauren <lauren.c4231@gmail.com>
2020-03-30 00:45:38 +0100

    Fix typo - add styles and nav

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
 
-const port = 3000;
+const port = 2345;
 
 const app = express();
 
 app.engine('handlebars', expHB({ defaultLayout: 'main' }));
-app.set('vie engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res, next) => {
-    res.render('searchusers');
+    res.render('search-users');
 });
 
 app.listen(port, () => {
