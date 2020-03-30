9bf768a0fe661a0b4752d66c330c6f23d4eea9cc
Lauren <lauren.c4231@gmail.com>
2020-03-30 01:43:30 +0100

    Add post request for creating user

--- app.js
+++ app.js
@@ -41,6 +41,39 @@ app.post('/user/search', (req, res, next) => {
     });
 });
 
+app.get('/user/add', (req, res, next) => {
+    res.render('add-user');
+});
+
+app.post('/user/add', (req, res, next) => {
+    let id = req.body.id;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+
+    client.hmset(
+        id,
+        [
+            'first_name',
+            first_name,
+            'last_name',
+            last_name,
+            'email',
+            email,
+            'phone',
+            phone
+        ],
+        (err, reply) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log(reply);
+            res.redirect('/');
+        }
+    );
+});
+
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
